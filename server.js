a3d00e09f073ba174dbfb9a5b81836fa80c73bea
Muslim050 <05-hitman@mail.ru>
2022-09-12 14:37:47 +0500

    BF

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const route = jsonServer.route("./public/db.json");
+const router = jsonServer.route("./public/db.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
 
 const PORT = process.env.PORT || 3001;
 server.use(middlewares);
-server.use(route);
+server.use(router);
 server.listen(PORT, () => {
   console.log("Server is running");
 });
